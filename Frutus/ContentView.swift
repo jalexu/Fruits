28586e23899c98392ff901ae0c2473dc5526f62d
Jaime Alexander Uribe <jaimealexanderuribe@gmail.com>
2020-10-16 14:12:07 -0500

    Initial Commit

---
+++ Frutus/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Frutus
+//
+//  Created by Jaime Uribe on 16/10/20.
+//  Copyright © 2020 Jaime Uribe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
